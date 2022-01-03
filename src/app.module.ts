93d133527ebb79e0107db685526d70e84b7e6555
Min Thet Maung <minthetmaung@nexlabs.co>
2022-01-04 00:47:34 +0630

    setup mongo database with env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { CoffeesModule } from './coffees/coffees.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/sample'),
+    MongooseModule.forRootAsync({
+      useFactory: async () => {
+        const { DATABASE_HOST, DATABASE_NAME, DATABASE_PORT } = process.env;
+        const uri = `mongodb://${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`;
+        return await { uri };
+      },
+    }),
     CoffeeRatingModule,
   ],
   controllers: [AppController],
